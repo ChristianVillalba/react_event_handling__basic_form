1af138b184257fba7557a523964908efaf4281d3
Christian Villalba UK <79871904+ChristianVillalba@users.noreply.github.com>
2021-08-12 14:05:44 +0100

    button change background color with mouse event: onMouseOver

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,28 @@ import React, { useState } from "react";
 
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
+  const [mouseOverStyle, setmouseOverStyle] = useState({
+    backgroundColor: "white"
+  });
 
   function handleClick() {
     setHeadingText("Submitted");
+  }
+  function handlemouseOver() {
+    setmouseOverStyle({ backgroundColor: "black" });
   }
 
   return (
     <div className="container">
       <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button onClick={handleClick}>Submit</button>
+      <button
+        onMouseOver={handlemouseOver}
+        style={mouseOverStyle}
+        onClick={handleClick}
+      >
+        Submit
+      </button>
     </div>
   );
 }
